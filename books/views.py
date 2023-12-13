0c25c7f45085f9322331affec9b621ddcf98cb93
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2023-12-13 17:42:23 -0300

    feat: sumario maded

--- books/views.py
+++ books/views.py
@@ -177,20 +177,20 @@ class ConvertDownloadBookView(generics.CreateAPIView):
 
             cap_text = f'<h1{cap}'
             cap_xml = f'''<?xml version="1.0" encoding="utf-8"?>
-                    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-                    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
-                    <html xmlns="http://www.w3.org/1999/xhtml">
-                    <head>
-                        <title>{cap_title}</title>
-                    </head>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>{cap_title}</title>
+</head>
 
-                    <body>
-                        <div class="editor ql-editor">
-                            {cap_text}
-                        </div>
-                    </body>
-                    </html>'''
+<body>
+    <div class="editor ql-editor">
+        {cap_text}
+    </div>
+</body>
+</html>'''
 
             cap_name = f'{book['author']}-{cap_counter}'
 
@@ -213,36 +213,21 @@ class ConvertDownloadBookView(generics.CreateAPIView):
         with open(f'{book_path}/page_styles.css', 'w') as f:
             f.write(page_style)
 
-        title_page = f"""<?xml version="1.0" encoding="utf-8"?>
-                <!DOCTYPE html>
+        title_page = f"""<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<!DOCTYPE html>
 
-                <html xmlns="http://www.w3.org/1999/xhtml"
-                xmlns:epub="http://www.idpf.org/2007/ops">
-                <head>
-                    <meta charset="utf-8"/>
-                    <meta name="viewport" content="width=454,height=652"/>
-                    <title>{book['title']}</title>
-                    <link href="stylesheet.css" rel="stylesheet"
-                    type="text/css"/>
-                </head>
-                <body id="{book['title']}" lang="pt-BR" xml:lang="pt-BR"
-                style="width:454px;height:652px">
-                    <div id="imgTitle" style='position:absolute;
-                    width: 454px; height: 653px;'>
-                            <img class='cover' src="{book['cover']}"
-                            alt=""/>
-                    </div>
-                    <div id="textTitle" style='position:absolute;
-                    width: 454px; height: 653px;display:flex; justify-content:
-                    center; align-items:center;flex-direction:column'>
-                            <h1>{book['title']}</h1>
-                            <h2>{book['subtitle']}</h2>
-                            <h2>{book['author']}</h2>
-                    </div>
-                </body>
-                </html>
-                </xml>
-                    """
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<head>
+  <title>{book['title']}</title>
+</head>
+<body>
+  <div style="height: 100vh; text-align: center; padding: 0pt; margin: 0pt;">
+    <svg xmlns="http://www.w3.org/2000/svg" height="100%" preserveAspectRatio="xMidYMid meet" version="1.1" viewBox="0 0 2879 4607" width="100%" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <image width="2879" height="4607" xlink:href="cover.jpeg"/>
+    </svg>
+  </div>
+</body>
+</html>"""
 
         Path(f'{book_path}/titlepage.xhtml').touch()
         with open(f'{book_path}/titlepage.xhtml', 'w') as f:
@@ -251,44 +236,67 @@ class ConvertDownloadBookView(generics.CreateAPIView):
         toc_list = []
 
         for cap in caps:
-            toc_list.append(f"""<navPoint class="chapter" id="{cap.name}"
-                            playOrder="{cap.cap_number}">
-                                    <navLabel><text>{cap.title}</text></navLabel>
-                                    <content src="{cap.name}.xhtml"/>
-                                </navPoint>
-                            """)
+            toc_list.append(f"""<navPoint class="chapter" id="{cap.name}" playOrder="{cap.cap_number}">
+    <navLabel><text>{cap.title}</text></navLabel>
+    <content src="{cap.name}.xhtml"/>
+</navPoint>""")
 
         toc_text = f"""<?xml version="1.0" encoding="UTF-8"?>
-                        <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
-                        "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
 
-                        <ncx version="2005-1" xml:lang="en" xmlns="http://www.daisy.org/z3986/2005/ncx/">
+<ncx version="2005-1" xml:lang="en" xmlns="http://www.daisy.org/z3986/2005/ncx/">
 
-                        <head>
-                            <meta name="dtb:uid" content="{book_uuid}"/>
-                            <meta name="dtb:depth" content="1"/>
-                            <meta name="dtb:totalPageCount" content="0"/>
-                            <meta name="dtb:maxPageNumber" content="0"/>
-                        </head>
+<head>
+    <meta name="dtb:uid" content="{book_uuid}"/>
+    <meta name="dtb:depth" content="1"/>
+    <meta name="dtb:totalPageCount" content="0"/>
+    <meta name="dtb:maxPageNumber" content="0"/>
+</head>
 
-                        <docTitle>
-                            <text>Sumário</text>
-                        </docTitle>
+<docTitle>
+    <text>Sumário</text>
+</docTitle>
 
-                        <docAuthor>
-                            <text>{book['author']}</text>
-                        </docAuthor>
+<docAuthor>
+    <text>{book['author']}</text>
+</docAuthor>
 
-                        <navMap>
-                        {''.join(str(cap) for cap in toc_list)}
-                        </navMap>
+<navMap>
+{''.join(str(cap) for cap in toc_list)}
+</navMap>
 
-                        </ncx>
-                    """
+</ncx>"""
 
         Path(f'{book_path}/toc.ncx').touch()
         with open(f'{book_path}/toc.ncx', 'w') as f:
             f.write(toc_text)
+
+        nav_list = []
+
+        for cap in caps:
+            nav_list.append(f"""<li>
+        <a href="{cap.name}.xhtml">{cap.title}</a>
+      </li>""")
+
+        nav_text = f"""<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
+<head>
+  <title>{book['title']}</title>
+  <meta charset="utf-8" /></head>
+<body epub:type="frontmatter">
+  <nav epub:type="toc" id="toc" role="doc-toc">
+    <h1>Sumário</h1>
+    <ol>
+      {''.join(str(cap) for cap in nav_list)}
+    </ol>
+  </nav>
+</body>
+</html>"""
+
+        Path(f'{book_path}/nav.xhtml').touch()
+        with open(f'{book_path}/nav.xhtml', 'w') as f:
+            f.write(nav_text)
 
         if not os.path.exists(f'{book_path}/META-INF'):
             os.mkdir(f'{book_path}/META-INF')
@@ -298,47 +306,51 @@ class ConvertDownloadBookView(generics.CreateAPIView):
         item_list = []
 
         for cap in caps:
-            item_list.append(f"""<item id='{cap.name}' href='{cap.name}.xhtml'
-                              media-type="application/xhtml+xml"/>""")
+            item_list.append(f"""<item id='{cap.name}' href='{
+                             cap.name}.xhtml' media-type="application/xhtml+xml"/>""")
 
         item_ref_list = []
 
         for cap in caps:
-            item_ref_list.append(f"""<itemref idref='{cap.name}' """)
+            item_ref_list.append(f"""<itemref idref='{cap.name}'/>""")
         meta = f"""<?xml version="1.0" encoding="utf-8"?>
-                    <package version="3.0" unique-identifier="bookid" prefix="rendition: http://www.idpf.org/vocab/rendition/# ibooks: http://vocabulary.itunes.apple.com/rdf/ibooks/vocabulary-extensions-1.0/" xmlns="http://www.idpf.org/2007/opf">
-                    <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
-                        <meta property="ibooks:specified-fonts">true</meta>
-                        <dc:title>{book['title']}</dc:title>
-                        <dc:creator id="cre">{book['author']}</dc:creator>
-                        <meta name="dtb:uid" content="{book_uuid}"/>
-                        <meta refines="#cre" property="role" scheme="marc:relators">aut</meta>
-                        <dc:date>{datetime.now()}</dc:date>
-                        <dc:language>pt-BR</dc:language>
-                        <meta property="dcterms:modified">2023-11-01T16:29:53Z</meta>
-                        <meta property="rendition:orientation">portrait</meta>
-                        <meta name="generator" content="Meu Novo Livro" />
-                        <meta name="cover" content="cover.jpeg" />
-                    </metadata>
-                    <manifest>
-                        <item id="titlepage" href="titlepage.xhtml" media-type="application/xhtml+xml"/>
-                        <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
-                        {''.join(str(cap) for cap in item_list)}
-                        <item id="page_styles.css" href="page_styles.css" media-type="text/css"/>
-                        <item id="stylesheet.css" href="stylesheet.css" media-type="text/css"/>
-                    </manifest>
-                    <spine toc="ncx">
-                        <itemref idref="ncx" linear="no"/>
-                        {''.join(str(cap) for cap in item_ref_list)}
-                    </spine>
-                        """
+<package version="3.0" unique-identifier="bookid" prefix="rendition: http://www.idpf.org/vocab/rendition/# ibooks: http://vocabulary.itunes.apple.com/rdf/ibooks/vocabulary-extensions-1.0/" xmlns="http://www.idpf.org/2007/opf">
+<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
+    <meta property="ibooks:specified-fonts">true</meta>
+    <dc:title>{book['title']}</dc:title>
+    <dc:creator id="cre">{book['author']}</dc:creator>
+    <meta name="dtb:uid" content="{book_uuid}"/>
+    <meta refines="#cre" property="role" scheme="marc:relators">aut</meta>
+    <dc:date>{datetime.now()}</dc:date>
+    <dc:language>pt-BR</dc:language>
+    <meta property="dcterms:modified">2023-11-01T16:29:53Z</meta>
+    <meta property="rendition:orientation">portrait</meta>
+    <meta name="generator" content="Meu Novo Livro" />
+    <meta name="cover" content="cover.jpeg" />
+</metadata>
+<manifest>
+    <item id="titlepage" href="titlepage.xhtml" media-type="application/xhtml+xml"/>
+    <item id="cover" href="cover.jpeg" media-type="image/jpeg" />
+    <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
+    {''.join(str(cap) for cap in item_list)}
+    <item id="page_styles.css" href="page_styles.css" media-type="text/css"/>
+    <item id="stylesheet.css" href="stylesheet.css" media-type="text/css"/>
+    <item id="nav.xhtml" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav"/>
+</manifest>
+<spine toc="ncx">
+    <itemref idref="cover" />
+    <itemref idref="nav" linear="no"/>
+    <itemref idref="ncx" linear="no"/>
+    {''.join(str(cap) for cap in item_ref_list)}
+</spine>
+</package>"""
         Path(f'{book_path}/content.opf').touch()
         with open(f'{book_path}/content.opf', 'w') as f:
             f.write(meta)
 
-        cover_path = f'{book['cover']}'
-
-        shutil.copy(cover_path[1:], f'{book_path}/cover.jpeg')
+        shutil.copy(
+            f'media/books/{book['name']}/cover.jpeg',
+            f'{book_path}/cover.jpeg')
 
         shutil.make_archive(f'{book_path}', 'zip', f'{book_path}')
         epub = Path(f'{book_path}.zip')
