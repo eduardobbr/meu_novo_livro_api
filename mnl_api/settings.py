d4efee3d80a2ddf8d4a1a90c38fa528a47fb77ce
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2024-04-11 15:19:25 -0300

    feat: add fly test

--- mnl_api/settings.py
+++ mnl_api/settings.py
@@ -17,6 +17,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+APP_NAME = os.environ.get("FLY_APP_NAME")
+ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]  # ← Updated!
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
